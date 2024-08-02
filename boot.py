1aebe7615a2df20718a5d4381ae87076863238a4
saurabh-2905 <saurabhband2905@gmail.com>
2024-08-02 10:44:42 +0200

    TO identify node id from boot

--- boot.py
+++ boot.py
@@ -11,8 +11,35 @@
 from machine import UART
 import machine
 import os
+import ustruct, ubinascii, uhashlib
 
 uart = UART(0, baudrate=115200)
 os.dupterm(uart)
 
-machine.main('main.py')
+# machine.main('main.py')
+
+
+
+'''
+Implemented of LoPy4/Fipy with 1.18 pycom-micropython version
+'''
+######### Utility Functions #######################
+def get_node_id(hex=False):
+    """
+    Get node id, which consists of four bytes unsigned int.
+    Return as hex, according to parameter.
+    """
+    node_id = ubinascii.hexlify(uhashlib.sha1(
+        machine.unique_id()).digest()).decode("utf-8")[-8:]
+    if hex:
+        return node_id
+    else:
+        return int(node_id, 16)
+
+######### Own node's information #####################
+if get_node_id() == 235968217:
+    print('I am node Mac1')
+elif get_node_id() == 829745241:
+    print('I am node Mac2')
+elif get_node_id() == 50989579:
+    print('I am node Mac3')
